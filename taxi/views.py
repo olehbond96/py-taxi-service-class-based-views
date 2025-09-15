4404485735f1f4c97cd6b1b786b12834f2623c59
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-09-15 21:23:55 +0200

    Implement home page, views, templates, pagination, sidebar; fix imports

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import get_user_model
 from taxi.models import Driver, Car, Manufacturer
+
+User = get_user_model()
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    queryset = (Car.objects.select_related
+                ("manufacturer").order_by
+                ("manufacturer__name", "model"))
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = User
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = User
+    queryset = User.objects.prefetch_related("cars__manufacturer")
 
 
 def index(request):
